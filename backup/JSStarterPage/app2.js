119efac24ede15683ed3c708c198240119b5b3cd
Darizola31 <Darizola31@gmail.com>
2015-01-28 20:40:25 -0600

    COMPLETE REVERT!!
    Copied over info from app.js in a different dir.

--- backup/JSStarterPage/app2.js
+++ backup/JSStarterPage/app2.js
@@ -1,13 +1,8 @@
 var main = function() {
-  $('.icon-menu').toggle(function() {    
-	$('.menu').animate({
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
       left: '0px'
     }, 200);
-	}, function() {
-	$('.menu').animate({
-      left: '-285px'
-    }, 200);
-});
 
     $('body').animate({
       left: '285px'
