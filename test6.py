0e8bd17ee9d0cea5a261960d41442b71c7a5c2f2
Darizola31 <Darizola31@users.noreply.github.com>
2018-10-31 21:40:28 -0500

    Add files via upload

--- test6.py
+++ test6.py
@@ -1,4 +1,25 @@
-from requests_html import HTMLSession
+import requests
+import getpass
+import json
+import urllib3
 
-session = HTMLSession()
-r = session.get('https://python.org/')
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+username = input("Username: ")
+password = getpass.getpass()
+
+print("-----------------")
+
+token_data=json.dumps({"username": username, "password": password})
+
+token = requests.post("https://securitycenter/rest/token", verify='\\pit-darizola\CA\Certificates-Current User\Personal\Certificates')
+#print(token.status_code)
+#print(token.json())
+#print(token.cookies)
+#print(token_num)
+token_num = token.json()['response']['token']
+
+headers={"X-SecurityCenter": str(token_num)}
+assets = requests.get("https://securitycenter/rest/asset?id=209&fields=id,name,viewableIPs", headers=headers,
+                      verify=False, cookies=token.cookies)
+
+print(assets.json())
